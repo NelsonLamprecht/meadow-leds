47e52c8da196fc1f9c5c8f76976c887f3fc98259
Nelson Lamprecht <Sorbitol77@outlook.com>
2022-08-21 20:01:52 -0500

    working on board heart beat and multi color led

---
+++ meadow_leds/meadow_leds/MeadowApp.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+
+namespace meadow_leds
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7FeatherV1, MeadowApp>
+    {
+        private RgbPwmLed _onboardLed;
+
+        public MeadowApp()
+        {
+            Initialize().Wait();
+
+            // heart beat
+            CycleOnboardLed(TimeSpan.FromMilliseconds(1000));
+
+            MultiColorLedController.Current.SetColor(Color.Green);
+
+        }
+
+        private async Task Initialize()
+        {
+            Console.WriteLine("Initialize hardware...");
+
+            _onboardLed = new RgbPwmLed(device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+
+            MultiColorLedController.Current.Initialize();
+
+            await Task.CompletedTask;
+        }
+
+        private void CycleOnboardLed(TimeSpan duration)
+        {
+            Console.WriteLine("Cycle colors...");
+
+            while (true)
+            {
+                ShowColor(Color.Red, duration);
+                ShowColor(Color.Blue, duration);
+            }
+        }
+
+        private void ShowColor(Color color, TimeSpan duration)
+        {
+            Console.WriteLine($"Color: {color}");
+            _onboardLed.SetColor(color);
+            Thread.Sleep(duration);
+            _onboardLed.Stop();
+        }
+    }
+}
---
+++ meadow_leds/meadow_leds/MultiColorLedController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Meadow.Foundation.Leds;
+using Meadow.Foundation;
+
+namespace meadow_leds
+{
+    internal class MultiColorLedController
+    {
+        private Random _random;
+        private RgbPwmLed _rgbPwmLed;
+        private Task _animationTask = null;
+        private CancellationTokenSource _cancellationTokenSource = null;        
+
+        protected bool Initialized = false;        
+
+        public static MultiColorLedController Current { get; private set; }
+
+        private MultiColorLedController()
+        {
+            _random = new Random();
+        }
+
+        static MultiColorLedController()
+        {
+            Current = new MultiColorLedController();
+        }
+
+        public void Initialize()
+        {
+            if (Initialized)
+            {
+                return;
+            }
+
+            _rgbPwmLed = new RgbPwmLed(
+                device: MeadowApp.Device,
+                redPwmPin: MeadowApp.Device.Pins.D12,
+                greenPwmPin: MeadowApp.Device.Pins.D11,
+                bluePwmPin: MeadowApp.Device.Pins.D10);
+            _rgbPwmLed.SetColor(Color.Red);
+
+            Initialized = true;
+
+            StartRunningColors();
+        }
+
+        private void Stop()
+        {
+            _rgbPwmLed.Stop();
+            _cancellationTokenSource?.Cancel();
+        }
+
+        public void SetColor(Color color)
+        {
+            Stop();
+            _rgbPwmLed.SetColor(color);
+        }
+
+        public void TurnOn()
+        {
+            Stop();
+            _rgbPwmLed.SetColor(GetRandomColor());
+            _rgbPwmLed.IsOn = true;
+        }
+
+        public void TurnOff()
+        {
+            Stop();
+            _rgbPwmLed.IsOn = false;
+        }
+
+        public void StartBlink()
+        {
+            Stop();
+            _rgbPwmLed.StartBlink(GetRandomColor());
+        }
+
+        public void StartPulse()
+        {
+            Stop();
+            _rgbPwmLed.StartPulse(GetRandomColor());
+        }
+
+        public void StartRunningColors()
+        {
+            _rgbPwmLed.Stop();
+
+            _animationTask = new Task(async () =>
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+                await StartRunningColors(_cancellationTokenSource.Token);
+            });
+            _animationTask.Start();
+        }
+
+        protected async Task StartRunningColors(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                _rgbPwmLed.SetColor(GetRandomColor());
+                await Task.Delay(1000);
+            }
+        }
+
+        protected Color GetRandomColor()
+        {
+            return Color.FromHsba(_random.NextDouble(), 1, 1);
+        }
+
+    }
+}
